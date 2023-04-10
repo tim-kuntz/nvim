00a89045a1e40585e26d13572e56371a2d3a81d6
Tim Kuntz <tim.kuntz@appfolio.com>
2023-04-10 08:29:54 -0500

    Add vim-dispatch for testing
    Added "tpope/vim-dispatch" as a dependency of "vim-test" and set
    the test strategy as 'dispatch'. This changes the test run behavior
    from running in a terminal to running in the quickfix window, which
    allows for quicker navigation and review when there are failures.

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -1,6 +1,14 @@
 return {
   "vim-test/vim-test",
 
+  dependencies = {
+    "tpope/vim-dispatch",
+  },
+
+  init = function()
+    vim.g['test#strategy'] = 'dispatch'
+  end,
+
   keys = {
     { "<leader>Ts", "<cmd>TestSuite<cr>", desc = "Test Suite" },
     { "<leader>Tf", "<cmd>TestFile<cr>",  desc = "Test File" },
@@ -8,4 +16,5 @@ return {
     { "<leader>Tl", "<cmd>TestLast<cr>", desc = "Test Last" },
     { "<leader>Tv", "<cmd>TestVisit<cr>", desc = "Test Visit" },
   },
+
 }
